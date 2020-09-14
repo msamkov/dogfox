6b2f0892801485fb6ad31a374d335b4b09e1dfa7
msamkov <littlebiggame2@gmail.com>
2020-09-14 16:43:32 +0300

    Basic аутентификация

---
+++ src/main/java/ru/multicon/dogfox/DogfoxApplication.java
@@ -0,0 +1,13 @@
+package ru.multicon.dogfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogfoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogfoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/multicon/dogfox/DogfoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.multicon.dogfox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogfoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
